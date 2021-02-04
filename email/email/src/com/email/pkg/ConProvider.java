6fb0b303d0fa63d7c8a210c9c373dfbdc4290fbf
Shirisha C A <65220435+Shirisha-CA@users.noreply.github.com>
2021-02-04 13:05:54 +0530

    Add files via upload

---
+++ email/email/src/com/email/pkg/ConProvider.java
@@ -0,0 +1,13 @@
+package com.email.pkg;
+import java.sql.*;
+public class ConProvider {
+
+	public static Connection getConnection(){
+	Connection con=null;
+	try{
+		Class.forName("com.mysql.jdbc.Driver");
+		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/email?user=root&password=root");
+	}catch(Exception e){System.out.println(e);}
+	return con;
+    }
+}
